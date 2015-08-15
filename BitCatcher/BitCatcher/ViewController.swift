85f56cfbc26c67b82c20aa6ef37134ee75de895e
Elizabeth Siegle <lizzie.siegle@gmail.com>
2015-08-15 06:15:17 -0700

    add braintree built in files

--- BitCatcher/BitCatcher/ViewController.swift
+++ BitCatcher/BitCatcher/ViewController.swift
@@ -15,8 +15,13 @@ class ViewController: UIViewController
     {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+       
+        
     }
-
+  
+    @IBOutlet weak var payBit: UIButton! {
+        
+    }
     override func didReceiveMemoryWarning()
     {
         super.didReceiveMemoryWarning()
